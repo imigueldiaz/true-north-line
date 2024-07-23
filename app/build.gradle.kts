4ba8d1527cebf0689a1d9dbc1e7f9307f53b01bb
Ignacio de Miguel Díaz <imigueldiaz@gmail.com>
2024-07-23 08:09:35 +0200

    Update Java and Kotlin source and target compatibility to version 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,11 +30,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11" // Updated to match the sourceCompatibility and targetCompatibility
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
